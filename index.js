0b15c4e4757d127fff26546ef9a442abe6cd56b9
ljylhm <948021695@qq.com>
2018-07-10 20:25:11 +0800

    工具函数的第一次提交

---
+++ index.js
@@ -0,0 +1,367 @@
+/**** 方法库 积累的常用的方法  ****/
+var axios = require("axios");
+// 时间转天数
+var name = "Ljy";
+var helper = {
+
+    /* 版本号 */
+    version: "0.0.2",
+    /************************* 数据检测类 *******************************/
+
+    /**
+     *@description 获得被检测的数据类型
+     *@param {any} obj 检测的数据
+     *@return {String} 被检测的数据类型 
+     */
+
+    getDataType: function (obj) {
+        var _type = "",
+            _type_str = "";
+
+        _type = Object.prototype.toString.call(obj);
+        _type_str = _type.substring(8, _type.length - 1);
+
+        return _type_str;
+    },
+
+    /**
+     *@description 检测是否为数字类型
+     *@param {number} num 检测的数据
+     *@return boolean 被检测的数据类型 
+     */
+
+    isNumber: function (num) { // 检查是否为数字类型
+        return this.getDataType(num) == "Number";
+    },
+
+    /**
+     * @description 检测是否为布尔类型
+     * @param {boolean} bol
+     * @returns boolean 被检测的数据类型 
+     */
+
+    isBoolean: function (bol: boolean) { // 检查是否为布尔类型
+        return this.getDataType(bol) == "Boolean";
+    },
+
+    /**
+     * @description 检测是否为布尔类型
+     * @param {String} str
+     * @returns 检查是否为字符串类型
+     */
+
+    isString: function (str: String) {
+        return this.getDataType(str) == "String";
+    },
+
+    /**
+     * @description 检测是否在浏览器的环境中
+     * @returns boolean
+     */
+
+    inBrowser: function () { // 检查是否在浏览器
+        return typeof window !== 'undefined';
+    },
+
+    /**
+     * @description 检测是否为空对象
+     * @param {Object} obj
+     * @returns {boolean}
+     * 这种方法不能检测dom的点击事件 
+     * 因为dom的点击事件会有一个e 
+     * 这个事件不能检测到 这个方法以后会做兼容
+    */
+
+    isEmptyObject: function (obj: Object) {
+        if (this.getDataType(obj) == "Arguments") {
+            if (obj.length <= 0) return true;
+            else return false
+        }
+        for (var key in obj) {
+            return false;
+        }
+        return true;
+    },
+
+    /**
+    * @description 检测 argruments 是否存在且不为零
+    * @returns {boolean}
+    */
+    argCheck: function () {
+        if (this.isEmptyObject(arguments)) return false;
+        for (var i in arguments) {
+            if (!arguments[i] && arguments[i] != 0) {
+                console.warn("name or value can not be null");
+                return false;
+            }
+        }
+        return true;
+    },
+
+    /************************* 浏览器存储 *******************************/
+
+    /**
+     * @description 对sessionStorage进行设置
+     * @param {String} name
+     * @param {String} value
+     */
+
+    sessionSet: function (name: String, value: String) {
+        if (!this.argCheck(name, value)) return;
+        sessionStorage.setItem(name, value);
+    },
+
+    sessionGet: function (name) {
+        if (!this.argCheck(name)) return window.sessionStorage;
+        return sessionStorage.getItem(name);
+    },
+
+    sessionClear: function (name) {
+        var sessionJson = window.sessionStorage;
+        if (!this.argCheck(name) && !this.isEmptyObject(sessionJson)) {
+            sessionJson.clear();
+        }
+        sessionJson.removeItem(name);
+    },
+
+    localStroageSet: function (name, value) {
+        if (!this.argCheck(name, value)) return;
+        localStorage.setItem(name, value);
+    },
+
+    localStroageGet: function (name) {
+        if (!this.argCheck(name)) return window.localStorage;
+        localStorage.getItem(name);
+    },
+
+    localStroageClear: function (name) {
+        var localStroageJson = window.localStroage;
+        if (!this.argCheck(name) && !this.isEmptyObject(localStroageJson)) {
+            localStroageJson.clear();
+        }
+        localStroageJson.removeItem(name);
+    },
+    /************************* 关于URL *******************************/
+    getUrl: function () { // 返回完整的url
+        return window.location.href;
+    },
+    getHash: function () {
+        var href = window.location.href
+        var index = href.indexOf('#')
+        return index === -1 ? '' : href.slice(index + 1)
+    },
+    setHashUrl: function (path) { // path为hash
+        var href = window.location.href
+        var i = href.indexOf('#')
+        var base = i >= 0 ? href.slice(0, i) : href
+        return base + "#" + path
+    },
+    /************************* 时间戳 *******************************/
+    // 时间戳转换为天数 时间戳皆转为毫秒
+    praseDays: function (timeStamp) {
+        var tDayStamp = 1000 * 60 * 60 * 24;
+        var tDiff = Math.floor(timeStamp / tDayStamp);
+        return tDiff;
+    },
+
+    // 获取时间戳
+    getTimeStamp: function (time) {
+        if (!time) return Math.round(new Date().getTime());
+        else {
+            if (this.getDataType(time) != "Date") {
+                return Math.round(new Date(time).getTime());
+            }
+            return Math.round(time.getTime());
+        }
+    },
+
+    // 转换为相差的天数 向下取整
+    transformDays: function (sDate1, sDate2) {
+        sDate2 = sDate2 ? sDate2 : new Date();
+        var _stamp1 = this.getTimeStamp(sDate1),
+            _stamp2 = this.getTimeStamp(sDate2);
+        return this.praseDays(Math.abs(_stamp1 - _stamp2));
+    },
+
+    /************************* http请求 *******************************/
+    // http 请求的拦截器 暴露一个请求的内容给cb 一定要有返回值
+    httpReqInterceptr: function (cb) {
+        axios.interceptors.request.use(cb)
+    },
+    // http 返回的拦截器 暴露一个返回的内容给cb 一定要有返回值
+    httpReqInterceptr: function (cb) {
+        axios.interceptors.response.use(cb)
+    },
+
+    http: function (method, url, data, options) {
+        method = method || "GET";
+        data = data || {};
+        options = options || {};
+        if (!url) console.error("[http]url is must");
+
+        var params = {
+            url: url,
+            method: method,
+            data: data,
+            timeout: 2000,
+        }
+
+        params = Object.assign(params, options);
+        return axios(params);
+    },
+
+    httpGet: function (url, data, opt) {
+        var method = 'get';
+        return this.http(method, url, data, opt);
+    },
+
+    httpPost: function (url, data, opt) {
+        var method = 'post';
+        return this.http(method, url, data, opt);
+    },
+
+
+    /************************* 字符串方法 *******************************/
+    // 获得对应字符串的数量
+    getCharNum: function (str1, str2) {
+        if (!this.argCheck(str1, str2)) return 0;
+        var count = 0,
+            reg = new RegExp(str2, "g");
+        str1.replace(reg, function () {
+            count++;
+        });
+        return count;
+    },
+
+    /************************* 对象的方法 *******************************/
+    // 判断是否两个对象是否相等
+    isEql: function (obj1, obj2) {
+        var _arr1 = [],
+            _arr2 = [];
+        var isFlag = true;
+        if (obj1 === obj2) return true;
+        if (helper.getDataType(obj1) != "Object") return false;
+        if (Object.keys(obj1).length != Object.keys(obj2).length) return false;
+        for (var i in obj1) {
+            if (!obj2.hasOwnProperty(i)) return false;
+            if (helper.getDataType(obj1[i]) != "Object") {
+                if (obj2[i] != obj1[i]) return false;
+            } else {
+                _arr1.push(obj1[i]);
+                _arr2.push(obj2[i]);
+            }
+        }
+        _arr1.forEach(function (ele, index, arr) {
+            isFlag = isFlag && isEql(_arr1[index], _arr2[index]);
+        });
+        return isFlag;
+    },
+
+    /************************* 数组的方法 *******************************/
+    // 数组深拷贝
+    deepClone: function (obj) {
+        var type = typeof obj == "object";
+        if (!type) return obj;
+        else {
+            var newObj = new Object();
+            for (var i in obj) {
+                newObj[i] = this.deepClone(obj[i]);
+            }
+            return newObj;
+        }
+    },
+    // 去除数组中重复的部分
+    clearRepeate: function (arr) {
+        return [...new Set(arr)];
+    },
+    // 去除数组中重复的部分
+    clearRepeateObj: function (arr, id) {
+        var obj = {};
+        var newArr = arr.reduce((pre, cur, index, arr) => {
+            obj[cur[id]] ? "" : obj[cur[id]] = true && pre.push(cur);
+            return pre;
+        }, [])
+        return newArr;
+    },
+    // 数组求和
+    arrSum: function (arr) {
+        return arr.reduce((pre, cur) => {
+            return pre + cur;
+        });
+    },
+    // 比较两个数组中不同的地方
+    difference: function (a, b) {
+        var s = new Set(a);
+        return b.filter((x) => {
+            !s.has(x);
+        })
+    },
+    /************************* 排序的方法 *******************************/
+    // 交换数组元素
+    swap: function (arr, index1, index2) {
+        var _tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = _tmp;
+    },
+    // 快速排序
+    quickSort: function (arr, left, right) {
+        if (arr.length < 2) return arr;
+        var _mid = arr.splice(0, 1)[0];
+
+        arr.forEach(function (ele, index, arr) {
+            if (ele < _mid) left.push(ele);
+            else right.push(ele);
+        });
+        return this.arrSort1(left, [], []).concat([_mid], this.arrSort1(right, [], []));
+    },
+    // 冒泡排序
+    bubbleSort: function (arr) {
+        for (var i = 0, len = arr.length; i < len; i++) {
+            for (var j = 0; j < len - i; j++) {
+                if (arr[j] > arr[j + 1]) this.swap(arr, j, j + 1)
+            }
+        }
+    },
+    // 选择排序
+    selectSort: function (arr) {
+        var _cursor = "";
+        for (var i = 0, len = arr.length; i < len; i++) {
+            _cursor = 0;
+            for (var j = 0; j < len - i; j++) {
+                _cursor = arr[j] > arr[_cursor] ? j : _cursor;
+            };
+            this.swap(arr, _cursor, len - i - 1);
+        }
+    },
+    /************************* Window的方法 *******************************/
+    scrollTop: function () { // 获取和设置距离顶部的距离
+        var c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (!this.argCheck(arguments)) return c;
+        var _arg = arguments[0];
+        if (_arg && this.isNumber(_arg)) document.documentElement.scrollTop = document.body.scrollTop = _arg;
+    },
+    scrollToTop: function () { // 当前位置滑动到顶部
+        var c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+            var _ = this;
+            window.requestAnimationFrame(function () { // requestAnimationFrame 本质其实是一个定时器);
+                _.scrollToTop(hight);
+            })
+            window.scrollTo(0, c - c / 16);
+        }
+    },
+    scrollToHeight: function (height) { // 当前位置滑动到顶部
+        var c = document.documentElement.scrollTop || document.body.scrollTop,
+            flag = c > height ? true : false, // "up" : "down"
+            _self = this;
+        if (Math.abs(c - height) > 20) {
+            window.requestAnimationFrame(function () {
+                _self.scrollToHeight(height);
+            })
+            if (flag) window.scrollTo(0, c - 20);
+            else window.scrollTo(0, c + 20);
+        }
+    }
+};
+
+module.exports = helper;
